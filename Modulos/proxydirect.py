d1a4dc2461c2c0ab8c5e97bca358e1ead66c2f8d
José Deodanes-code <60304928+Jose-developer-start@users.noreply.github.com>
2020-12-25 23:44:40 -0600

    UPDATE

--- Modulos/proxydirect.py
+++ Modulos/proxydirect.py
@@ -248,7 +248,7 @@ def parse_args(argv):
 
 
 def main(host=LISTENING_ADDR, port=LISTENING_PORT):
-    print "\n:-------PythonProxy-------:\n"
+    print "\n:-------Python Proxy-------:\n"
     print "Listening addr: " + LISTENING_ADDR
     print "Listening port: " + str(LISTENING_PORT) + "\n"
     print ":-------------------------:\n"
